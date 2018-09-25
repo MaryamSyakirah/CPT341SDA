e7913a26d2835a22179b48f5afbf47f5cd7d7dd8
MaryamSyakirah <43544475+MaryamSyakirah@users.noreply.github.com>
2018-09-26 00:06:51 +0800

    First change

--- CPT341SDA-master/CoffeeMaker/CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/Main.java
+++ CPT341SDA-master/CoffeeMaker/CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/Main.java
@@ -20,6 +20,8 @@ public class Main {
      * Prints the main menu and handles user input for 
      * main menu commands.
      */
+    
+    // Try comment........
     public static void mainMenu() {
         System.out.println("1. Add a recipe");
         System.out.println("2. Delete a recipe");
